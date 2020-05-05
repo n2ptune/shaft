421a9ebd509e80a61d8c9b1acf457bfb6b3f99b3
n2ptune <n2ptune@outlook.com>
2020-05-05 18:24:27 +0900

    working too hard

--- api/models/user/create.js
+++ api/models/user/create.js
@@ -1,4 +1,10 @@
+import bcrypt from 'bcrypt'
 import db from '../../db/connection'
+
+export const validatePassword = (password) => {
+  // not finished
+  return !!password
+}
 
 /**
  * Insert information of user into user table
@@ -7,4 +13,22 @@ import db from '../../db/connection'
  * @param {String} password
  * @param {Function} cb
  */
-export const createUser = (nickname, email, password, cb) => {}
+export const createUser = async (nickname, email, password, cb) => {
+  // validate password
+  if (validatePassword(password)) {
+    const sql = `INSERT INTO ${process.env.DB_USER_TABLE} (nickname, email, password) VALUES (?, ?, ?)`
+    const saltRounds = 8
+
+    try {
+      const salt = bcrypt.genSaltSync(saltRounds)
+      const hash = bcrypt.hashSync(password, salt)
+
+      await db.query(sql, [nickname, email, hash])
+      cb(null, true)
+    } catch (error) {
+      cb(new Error(error.message), false)
+    }
+  } else {
+    cb(new Error('패스워드 검증 실패'), false)
+  }
+}
--- api/routes/auth/sign-up.js
+++ api/routes/auth/sign-up.js
@@ -1,4 +1,5 @@
 import { validateNicknameAndEmail } from '../../models/user/validate'
+import { createUser } from '../../models/user/create'
 
 export default function(req, res) {
   const { nickname, email, password } = req.body.data
@@ -19,8 +20,18 @@ export default function(req, res) {
         message: '닉네임 혹은 이메일이 이미 존재합니다.'
       })
     } else {
-      res.status(200).send({
-        message: '아이디 / 이메일 / 비밀번호 생성 가능'
+      // res.status(200).send({
+      //   message: '아이디 / 이메일 / 비밀번호 생성 가능'
+      // })
+      // TODO add logic that store user data to table
+      createUser(nickname, email, password, (error, success) => {
+        if (error || !success) {
+          res.status(500).send({
+            message: '회원가입 불가능'
+          })
+        } else {
+          res.status(201).end()
+        }
       })
     }
   })
