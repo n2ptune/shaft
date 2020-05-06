afb4cc5b22e6b62464f95a59317d65e305b388d5
n2ptune <n2ptune@outlook.com>
2020-05-06 21:41:51 +0900

    working so hard

--- api/routes/auth/sign-up.js
+++ api/routes/auth/sign-up.js
@@ -20,10 +20,6 @@ export default function(req, res) {
         message: '닉네임 혹은 이메일이 이미 존재합니다.'
       })
     } else {
-      // res.status(200).send({
-      //   message: '아이디 / 이메일 / 비밀번호 생성 가능'
-      // })
-      // TODO add logic that store user data to table
       createUser(nickname, email, password, (error, success) => {
         if (error || !success) {
           res.status(500).send({
