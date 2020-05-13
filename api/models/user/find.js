ffea77f95859509caf6c7c4e9640b82a30abaa7a
n2ptune <n2ptune@outlook.com>
2020-05-13 17:09:03 +0900

    test find user by email

--- api/models/user/find.js
+++ api/models/user/find.js
@@ -1,9 +1,32 @@
 import db from '../../db/connection'
+import 'dotenv/config'
 
 export const findByEmail = async (email) => {
   if (!email) return
 
   try {
-    const [rows] = await db.query('SELECT * FROM')
+    const [
+      rows
+    ] = await db.query(
+      `SELECT * FROM ${process.env.DB_USER_TABLE} WHERE email = ?`,
+      [email]
+    )
+    if (!rows.length) {
+      throw new Error('Not Found Email')
+    }
+    return rows[0]
+  } catch (error) {
+    return error
   }
 }
+
+async function test(email) {
+  try {
+    const result = await findByEmail(email)
+    console.log(result)
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+test('n2ptune@outlook.com')
