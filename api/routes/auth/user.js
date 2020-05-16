a108162530f9a0c81f07b06cb49d903813f592a9
n2ptune <n2ptune@outlook.com>
2020-05-16 22:15:13 +0900

    authorization headers

--- api/routes/auth/user.js
+++ api/routes/auth/user.js
@@ -1,12 +1,21 @@
 import { decodeToken } from '../../models/user/token'
 
 export default function(req, res) {
-  const { token } = req.body.data
-  const decoded = decodeToken(token)
+  try {
+    if (!req.headers.authorization) throw new Error('Not Found AccessToken')
 
-  if (decoded) {
-    res.status(200).send(decoded)
-  } else {
-    res.status(400).send({ message: 'error' })
+    const token = req.headers.authorization.replace('Bearer ', '')
+    const decoded = decodeToken(token)
+
+    if (!decoded) throw new Error('Invalid Token')
+
+    return res.status(200).send(decoded)
+  } catch (error) {
+    return res.status(400).send({ message: error.message })
   }
+  // if (decoded) {
+  //   res.status(200).send(decoded)
+  // } else {
+  //   res.status(400).send({ message: 'error' })
+  // }
 }
--- store/index.js
+++ store/index.js
@@ -2,14 +2,19 @@ import axios from 'axios'
 
 export const actions = {
   nuxtClientInit({ commit, dispatch }) {
-    dispatch('getUserDataWithAccessToken')
+    const token = localStorage.getItem('at')
+
+    if (token) {
+      dispatch('getUserDataWithAccessToken')
+    }
   },
   async getUserDataWithAccessToken({ commit }) {
     const token = localStorage.getItem('at')
-    const { data: userData } = await axios.post('/api/auth/user', {
-      data: { token }
+    const { data: userData } = await axios.post('/api/auth/user', null, {
+      headers: {
+        Authorization: 'Bearer ' + token
+      }
     })
-
     commit('auth/setUser', userData)
   }
 }
