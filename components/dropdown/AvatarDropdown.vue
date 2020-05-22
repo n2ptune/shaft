ab62ace520c6731307c4024dcec5c0f45ce6877f
n2ptune <n2ptune@outlook.com>
2020-05-22 20:01:45 +0900

    avatar dropdown

---
+++ components/dropdown/AvatarDropdown.vue
@@ -0,0 +1,37 @@
+<template>
+  <transition name="fade" appear>
+    <div class="dropdown">
+      <ul class="dropdown-list">
+        <li class="dropdown-item">
+          새 1
+        </li>
+      </ul>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  created() {}
+}
+</script>
+
+<style lang="postcss" scoped>
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.3s ease;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+.fade-enter-to,
+.fade-leave {
+  opacity: 1;
+}
+.dropdown {
+  top: 45px;
+  transform: translateX(-30%);
+  @apply absolute max-w-xl bg-white rounded p-3 shadow border border-gray-300;
+}
+</style>
--- components/layouts/default/Header.vue
+++ components/layouts/default/Header.vue
@@ -2,19 +2,20 @@
   <header class="p-4 border-b border-gray-300">
     <div class="max-w-xl mx-auto flex justify-between items-center">
       <div class="font-bold text-xl">Header</div>
-      <div class="mx-6">
+      <div>
         <ClientOnly>
-          <div v-if="isAuth && user" class="auth">
-            <nuxt-link :to="`/users/${user.id}`">
-              <img
-                :src="user.avatar"
-                :style="{ width: '40px' }"
-                class="rounded-full"
-              />
-            </nuxt-link>
-            <div class="">
-              <button @click="logout">!!!</button>
-            </div>
+          <div
+            v-if="isAuth && user"
+            class="auth"
+            @mouseover="isDropdown = true"
+            @mouseleave="isDropdown = false"
+          >
+            <img
+              :src="user.avatar"
+              :style="{ width: '40px' }"
+              class="rounded-full"
+            />
+            <AvatarDropdown v-if="isDropdown" />
           </div>
           <div v-else class="auth">
             <nuxt-link to="/auth/sign-in">
@@ -32,8 +33,17 @@
 
 <script>
 import { mapGetters, mapActions } from 'vuex'
+import AvatarDropdown from '@/components/dropdown/AvatarDropdown'
 
 export default {
+  components: {
+    AvatarDropdown
+  },
+
+  data: () => ({
+    isDropdown: false
+  }),
+
   computed: {
     ...mapGetters({
       isAuth: 'auth/getIsLogin',
