06920504c09c8550357af36ec7ebfbcbb70d8ed4
n2ptune <n2ptune@outlook.com>
2020-05-25 19:40:17 +0900

    auth

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,10 +1,8 @@
 export default function({ store, redirect }) {
   if (!process.server) {
     if (!store.getters['auth/getIsLogin']) {
-      console.log('not authed')
       return redirect('/auth/sign-in')
     } else {
-      console.log('authed')
     }
   }
 }
