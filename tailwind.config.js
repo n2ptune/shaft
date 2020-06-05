03ef0e41ea243362915888d6845151146a90a7d0
n2ptune <n2ptune@outlook.com>
2020-06-05 19:40:56 +0900

    changed target of purgecss

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   purge: {
     enabled: process.env.NODE_ENV === 'production',
-    content: ['./src/**/*.html', './src/**/*.vue', './src/**/*.js']
+    content: ['./components/**/*.vue', './layouts/**/*.vue', './pages/**/*.vue']
   },
   plugins: [
     function({ addComponents }) {
