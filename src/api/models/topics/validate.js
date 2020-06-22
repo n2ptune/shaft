0bedacd86252c3f407479475edb1251907ba25e4
n2ptune <n2ptune@outlook.com>
2020-06-22 16:03:10 +0900

    fix: incorrect regexp on function of validate topics

--- src/api/models/topics/validate.js
+++ src/api/models/topics/validate.js
@@ -5,7 +5,7 @@ export const MIN_CONTENT_LENGTH = 10
  * @param {String} date
  */
 function validateDate(date) {
-  return /[1-2][0-9]{3}-[0-1][0-9]-[0-1][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]/.test(
+  return /[1-2][0-9]{3}-[0-1][0-9]-[0-3][0-9] [0-2][0-9]:[0-5][0-9]:[0-5][0-9]/.test(
     date
   )
 }
